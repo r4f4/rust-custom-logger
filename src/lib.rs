8a6c05b5bd8db2ee58ab34132129ccd5695f6e2e
Rafael Fonseca <r4f4rfs@gmail.com>
2025-01-08 22:14:52 +0100

    Add `with_level` builder and make log_level private

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub enum Level {
 
 //#[derive(Default)]
 pub struct Logging {
-    pub log_level: Level,
+    log_level: Level,
 }
 
 impl Logging {
@@ -19,6 +19,15 @@ impl Logging {
         let dt = Local::now();
         let dt_new = DateTime::<Local>::from_naive_utc_and_offset(dt.naive_utc(), *dt.offset());
         dt_new.format("%Y-%m-%d %H:%M:%S%.3f").to_string()
+    }
+
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn with_level(mut self, level: Level) -> Self {
+        self.log_level = level;
+        self
     }
 
     // info
@@ -124,6 +133,14 @@ impl Logging {
     }
 }
 
+impl Default for Logging {
+    fn default() -> Self {
+        Self {
+            log_level: Level::INFO,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     // this brings everything from parent's scope into this scope
@@ -131,98 +148,74 @@ mod tests {
 
     #[test]
     fn test_info_pass() {
-        let log = &Logging {
-            log_level: Level::INFO,
-        };
+        let log = Logging::new();
         log.info("testing info logging");
     }
 
     #[test]
     fn test_debug_pass() {
-        let log = &Logging {
-            log_level: Level::DEBUG,
-        };
+        let log = Logging::new().with_level(Level::DEBUG);
         log.debug("testing debug logging");
     }
 
     #[test]
     fn test_trace_pass() {
-        let log = &Logging {
-            log_level: Level::TRACE,
-        };
+        let log = Logging::new().with_level(Level::TRACE);
         log.trace("testing trace logging");
     }
 
     #[test]
     fn test_warn_pass() {
-        let log = &Logging {
-            log_level: Level::WARN,
-        };
+        let log = Logging::new().with_level(Level::WARN);
         log.warn("testing warn logging");
     }
 
     #[test]
     fn test_mid_pass() {
-        let log = &Logging {
-            log_level: Level::INFO,
-        };
+        let log = Logging::new().with_level(Level::INFO);
         log.mid("testing mid logging");
     }
 
     #[test]
     fn test_hi_pass() {
-        let log = &Logging {
-            log_level: Level::INFO,
-        };
+        let log = Logging::new();
         log.hi("testing hi logging");
     }
 
     #[test]
     fn test_lo_pass() {
-        let log = &Logging {
-            log_level: Level::INFO,
-        };
+        let log = Logging::new();
         log.lo("testing lo logging");
     }
 
     #[test]
     fn test_ex_pass() {
-        let log = &Logging {
-            log_level: Level::INFO,
-        };
+        let log = Logging::new();
         log.ex("testing ex logging");
     }
 
     #[test]
     fn test_error_pass() {
-        let log = &Logging {
-            log_level: Level::INFO,
-        };
+        let log = Logging::new();
         log.error("testing error logging");
     }
 
     // set level to TRACE
     #[test]
     fn test_mid_with_trace_pass() {
-        let log = &Logging {
-            log_level: Level::TRACE,
-        };
+        let log = Logging::new().with_level(Level::TRACE);
         log.mid("testing mid logging with TRACE");
     }
 
     #[test]
     fn test_hi_with_trace_pass() {
-        let log = &Logging {
-            log_level: Level::TRACE,
-        };
+        let log = Logging::new().with_level(Level::TRACE);
         log.hi("testing hi logging with TRACE");
     }
 
     #[test]
     fn test_display_types() {
-        let log = Logging {
-            log_level: Level::INFO,
-        };
+        let log = Logging::new();
         log.info(format!("testing logging with format: {}", true));
         log.info(String::from("testing logging with String"));
         log.info(1);
