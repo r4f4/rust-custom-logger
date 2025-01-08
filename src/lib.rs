0de6bf89d1d50b777ccc14170cecfdf6cdbe26cb
Rafael Fonseca <r4f4rfs@gmail.com>
2025-01-08 21:50:42 +0100

    Move timestamp formatting to common function

--- src/lib.rs
+++ src/lib.rs
@@ -15,34 +15,34 @@ pub struct Logging {
 }
 
 impl Logging {
+    fn timestamp(&self) -> String {
+        let dt = Local::now();
+        let dt_new = DateTime::<Local>::from_naive_utc_and_offset(dt.naive_utc(), *dt.offset());
+        dt_new.format("%Y-%m-%d %H:%M:%S%.3f").to_string()
+    }
+
     // info
     pub fn info(&self, msg: &str) {
         if self.log_level == Level::INFO
             || self.log_level == Level::DEBUG
             || self.log_level == Level::TRACE
         {
-            let dt = Local::now();
-            let naive_utc = dt.naive_utc();
-            let offset = dt.offset().clone();
-            let dt_new = DateTime::<Local>::from_naive_utc_and_offset(naive_utc, offset);
-            let dt_formated = dt_new.format("%Y-%m-%d %H:%M:%S%.3f");
             println!(
                 "\x1b[1;94m [ {} {} ] \x1b[0m  : {}",
-                "INFO ", dt_formated, msg
+                "INFO ",
+                self.timestamp(),
+                msg
             );
         }
     }
     /// debug
     pub fn debug(&self, msg: &str) {
-        let dt = Local::now();
-        let naive_utc = dt.naive_utc();
-        let offset = dt.offset().clone();
-        let dt_new = DateTime::<Local>::from_naive_utc_and_offset(naive_utc, offset);
-        let dt_formated = dt_new.format("%Y-%m-%d %H:%M:%S%.3f");
         if self.log_level == Level::DEBUG || self.log_level == Level::TRACE {
             println!(
                 "\x1b[1;92m [ {} {} ] \x1b[0m  : {}",
-                "DEBUG", dt_formated, msg
+                "DEBUG",
+                self.timestamp(),
+                msg
             );
         }
     }
@@ -52,14 +52,11 @@ impl Logging {
             || self.log_level == Level::DEBUG
             || self.log_level == Level::TRACE
         {
-            let dt = Local::now();
-            let naive_utc = dt.naive_utc();
-            let offset = dt.offset().clone();
-            let dt_new = DateTime::<Local>::from_naive_utc_and_offset(naive_utc, offset);
-            let dt_formated = dt_new.format("%Y-%m-%d %H:%M:%S%.3f");
             println!(
                 "\x1b[1;94m [ {}  {} ] \x1b[0m  : \x1b[1;93m{} \x1b[0m",
-                "INFO", dt_formated, msg
+                "INFO",
+                self.timestamp(),
+                msg
             );
         }
     }
@@ -69,14 +66,11 @@ impl Logging {
             || self.log_level == Level::DEBUG
             || self.log_level == Level::TRACE
         {
-            let dt = Local::now();
-            let naive_utc = dt.naive_utc();
-            let offset = dt.offset().clone();
-            let dt_new = DateTime::<Local>::from_naive_utc_and_offset(naive_utc, offset);
-            let dt_formated = dt_new.format("%Y-%m-%d %H:%M:%S%.3f");
             println!(
                 "\x1b[1;94m [ {}  {} ]  \x1b[0m : \x1b[1;94m{} \x1b[0m",
-                "INFO", dt_formated, msg
+                "INFO",
+                self.timestamp(),
+                msg
             );
         }
     }
@@ -86,14 +80,11 @@ impl Logging {
             || self.log_level == Level::DEBUG
             || self.log_level == Level::TRACE
         {
-            let dt = Local::now();
-            let naive_utc = dt.naive_utc();
-            let offset = dt.offset().clone();
-            let dt_new = DateTime::<Local>::from_naive_utc_and_offset(naive_utc, offset);
-            let dt_formated = dt_new.format("%Y-%m-%d %H:%M:%S%.3f");
             println!(
                 "\x1b[1;94m [ {}  {} ]  \x1b[0m : \x1b[1;95m{} \x1b[0m",
-                "INFO", dt_formated, msg
+                "INFO",
+                self.timestamp(),
+                msg
             );
         }
     }
@@ -103,53 +94,41 @@ impl Logging {
             || self.log_level == Level::DEBUG
             || self.log_level == Level::TRACE
         {
-            let dt = Local::now();
-            let naive_utc = dt.naive_utc();
-            let offset = dt.offset().clone();
-            let dt_new = DateTime::<Local>::from_naive_utc_and_offset(naive_utc, offset);
-            let dt_formated = dt_new.format("%Y-%m-%d %H:%M:%S%.3f");
             println!(
                 "\x1b[1;94m [ {}  {} ]  \x1b[0m : \x1b[1;98m{} \x1b[0m",
-                "INFO", dt_formated, msg
+                "INFO",
+                self.timestamp(),
+                msg
             );
         }
     }
     /// trace
     pub fn trace(&self, msg: &str) {
-        let dt = Local::now();
-        let naive_utc = dt.naive_utc();
-        let offset = dt.offset().clone();
-        let dt_new = DateTime::<Local>::from_naive_utc_and_offset(naive_utc, offset);
-        let dt_formated = dt_new.format("%Y-%m-%d %H:%M:%S%.3f");
         if self.log_level == Level::TRACE {
             println!(
                 "\x1b[1;96m [ {} {} ] \x1b[0m  : {}",
-                "TRACE", dt_formated, msg
+                "TRACE",
+                self.timestamp(),
+                msg
             );
         }
     }
     /// warning
     pub fn warn(&self, msg: &str) {
-        let dt = Local::now();
-        let naive_utc = dt.naive_utc();
-        let offset = dt.offset().clone();
-        let dt_new = DateTime::<Local>::from_naive_utc_and_offset(naive_utc, offset);
-        let dt_formated = dt_new.format("%Y-%m-%d %H:%M:%S%.3f");
         println!(
             "\x1b[1;93m [ {}  {} ] \x1b[0m  : {}",
-            "WARN", dt_formated, msg
+            "WARN",
+            self.timestamp(),
+            msg
         );
     }
     /// error
     pub fn error(&self, msg: &str) {
-        let dt = Local::now();
-        let naive_utc = dt.naive_utc();
-        let offset = dt.offset().clone();
-        let dt_new = DateTime::<Local>::from_naive_utc_and_offset(naive_utc, offset);
-        let dt_formated = dt_new.format("%Y-%m-%d %H:%M:%S%.3f");
         println!(
             "\x1b[1;91m [ {} {} ] \x1b[0m  : {}",
-            "ERROR", dt_formated, msg
+            "ERROR",
+            self.timestamp(),
+            msg
         );
     }
 }
